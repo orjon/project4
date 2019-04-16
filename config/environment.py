b89c3d539256a645ce32defdafd0d6e8f255d153
orjon <github@orjon.com>
2019-04-16 14:39:04 +0100

    deployment test1

--- config/environment.py
+++ config/environment.py
@@ -1,2 +1,3 @@
 import os #for eventual production
 secret = os.getenv('SECRET', 'project4secret')
+db_uri = os.getenv('DATABASE_URL', 'postgres://localhost:5432/db')
--- config/routes.py
+++ config/routes.py
@@ -1,3 +1,4 @@
+import os
 from app import app
 from controllers import projects, clients, invoices, expenses, suppliers, auth, users
 
@@ -8,3 +9,12 @@ app.register_blueprint(expenses.api, url_prefix='/api')
 app.register_blueprint(suppliers.api, url_prefix='/api')
 app.register_blueprint(auth.api, url_prefix='/api')
 app.register_blueprint(users.api, url_prefix='/api')
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+
+    if os.path.isfile('dist/' + path):
+        return app.send_static_file(path)
+
+    return app.send_static_file('index.html')
